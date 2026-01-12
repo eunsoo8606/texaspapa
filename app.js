35db93ef0ccaebae523e158df444f42d95b3ae10
eunsoo8606 <eunsoo8606@gmail.com>
2026-01-12 23:26:23 +0900

    feat : 공지사항 페이지 개발

--- app.js
+++ app.js
@@ -12,27 +12,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // 라우팅
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Texas Papa - Welcome' });
+    res.render('franchise', { title: 'Texas Papa - Franchise', activePage: 'franchise' });
 });
 
 app.get('/brand', (req, res) => {
-    res.render('brand', { title: 'Texas Papa - Brand' });
+    res.render('brand', { title: 'Texas Papa - Brand', activePage: 'brand' });
 });
 
 app.get('/company', (req, res) => {
-    res.render('company', { title: 'Texas Papa - Company' });
+    res.render('company', { title: 'Texas Papa - Company', activePage: 'brand' });
 });
 
 app.get('/menu', (req, res) => {
-    res.render('menu', { title: 'Texas Papa - Menu' });
+    res.render('menu', { title: 'Texas Papa - Menu', activePage: 'menu' });
 });
 
 app.get('/franchise', (req, res) => {
-    res.render('franchise', { title: 'Texas Papa - Franchise' });
+    res.render('franchise', { title: 'Texas Papa - Franchise', activePage: 'franchise' });
+});
+
+// 커뮤니티 섹션 라우팅 (단일 페이지)
+app.get('/community/:tab?', (req, res) => {
+    const tab = req.params.tab || 'notice'; // 기본값은 공지사항
+    const titles = {
+        notice: '공지사항',
+        event: '이벤트',
+        faq: 'FAQ',
+        voice: '고객의소리',
+        inquiry: '문의게시판'
+    };
+
+    res.render('community/index', {
+        title: `${titles[tab] || '커뮤니티'} | Texas Papa`,
+        activePage: 'community',
+        currentTab: tab
+    });
 });
 
 app.get('/location', (req, res) => {
-    res.render('location', { title: 'Texas Papa - Location' });
+    res.render('location', { title: 'Texas Papa - Location', activePage: 'brand' });
 });
 
 if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
