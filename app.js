4767b0ac60a12af77885cee24b6ce3737f07ae49
eunsoo8606 <eunsoo8606@gmail.com>
2026-02-08 13:23:53 +0900

    feat: 관리자 게시판 작성 및 수정 페이지와 Quill 에디터 통합 기능을 추가했습니다.

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('trust proxy', 1);
 
 // 미들웨어 설정
-app.use(compression());
 app.use(express.urlencoded({ limit: '50mb', extended: true })); // POST 데이터 파싱
 app.use(express.json({ limit: '50mb' })); // JSON 데이터 파싱
+app.use(compression());
 
 // MySQL 세션 스토어 설정 (Vercel 서버리스 환경 대응)
 const sessionStoreOptions = {
